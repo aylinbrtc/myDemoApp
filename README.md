80830f20bc04e1a6b7ca20d7316032c259c3f2e6
aylinbrtc <a.barutcu@etu.edu.tr>
2024-03-12 16:11:36 +0300

    Added demo site to README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Demo site: https://git.heroku.com/fierce-river-35208.git
+
+
 [![Build Status](https://app.travis-ci.com/aylinbrtc/myDemoApp.svg?token=zsUqFb3HbguQjV6DMTxg&branch=master)](https://app.travis-ci.com/aylinbrtc/myDemoApp)
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,6 +15,52 @@ import static spark.Spark.*;
 
 public class App {
 
+    public ArrayList Decoder(ArrayList<Integer> intArray1, ArrayList<Integer> intArray2, ArrayList<String> stringArray, int value) {
+        int sum = 0;
+
+        if (intArray1 == null || intArray2 == null || stringArray == null) {
+            throw new IllegalArgumentException("ArrayList'ler null olamaz");
+        }
+
+        ArrayList<String> modifiedStrings = new ArrayList<>();
+
+        // String arraylistinin uzunluğu kadar döngü
+        for (int i = 0; i < stringArray.size(); i++) {
+            sum = 0;
+            int index1 = i % intArray1.size(); // İlk int arraylistinin uzunluğuna göre mod al
+            int index2 = i % intArray2.size(); // İkinci int arraylistinin uzunluğuna göre mod al
+
+            // İlgili indeksteki değerleri topla
+            sum = intArray1.get(index1) + intArray2.get(index2);
+            String word = stringArray.get(i);
+            StringBuilder result = new StringBuilder();
+            for (int j = 0; j < word.length(); j++) {
+                char ch = word.charAt(j);
+                char newCh = shiftChar(ch, sum + j * value);
+                result.append(newCh);
+            }
+            modifiedStrings.add(result.toString());
+        }
+        return modifiedStrings;
+    }
+
+    public static char shiftChar(char ch, int shiftValue) {
+        if (Character.isDigit(ch)) { // Karakter bir sayıysa
+            int digit = Character.getNumericValue(ch);
+            int shiftedDigit = (digit + shiftValue) % 10; // 0-9 arasında bir döngü oluştur
+            return Character.forDigit(shiftedDigit, 10);
+        } else if (Character.isLowerCase(ch)) { // Karakter bir küçük harfse
+            int offset = 'a'; // ASCII'de küçük harfler a'dan başlar
+            int shiftedChar = ((ch - offset + shiftValue) % 26 + 26) % 26 + offset; // Küçük harfler arasında bir döngü oluştur
+            return (char) shiftedChar;
+        } else if (Character.isUpperCase(ch)) { // Karakter bir büyük harfse
+            int offset = 'A'; // ASCII'de büyük harfler A'dan başlar
+            int shiftedChar = ((ch - offset + shiftValue) % 26 + 26) % 26 + offset; // Büyük harfler arasında bir döngü oluştur
+            return (char) shiftedChar;
+        } else { // Karakter bir harf veya sayı değilse
+            throw new IllegalArgumentException("Invalid character: " + ch);
+        }
+    }
     
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
@@ -79,51 +125,6 @@ public class App {
         }
         return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
     }
-    public ArrayList Decoder(ArrayList<Integer> intArray1, ArrayList<Integer> intArray2, ArrayList<String> stringArray, int value) {
-        int sum = 0;
-
-        if (intArray1 == null || intArray2 == null || stringArray == null) {
-            throw new IllegalArgumentException("ArrayList'ler null olamaz");
-        }
-
-        ArrayList<String> modifiedStrings = new ArrayList<>();
-
-        // String arraylistinin uzunluğu kadar döngü
-        for (int i = 0; i < stringArray.size(); i++) {
-            sum = 0;
-            int index1 = i % intArray1.size(); // İlk int arraylistinin uzunluğuna göre mod al
-            int index2 = i % intArray2.size(); // İkinci int arraylistinin uzunluğuna göre mod al
-
-            // İlgili indeksteki değerleri topla
-            sum = intArray1.get(index1) + intArray2.get(index2);
-            String word = stringArray.get(i);
-            StringBuilder result = new StringBuilder();
-            for (int j = 0; j < word.length(); j++) {
-                char ch = word.charAt(j);
-                char newCh = shiftChar(ch, sum + j * value);
-                result.append(newCh);
-            }
-            modifiedStrings.add(result.toString());
-        }
-        return modifiedStrings;
-    }
-
-    public static char shiftChar(char ch, int shiftValue) {
-        if (Character.isDigit(ch)) { // Karakter bir sayıysa
-            int digit = Character.getNumericValue(ch);
-            int shiftedDigit = (digit + shiftValue) % 10; // 0-9 arasında bir döngü oluştur
-            return Character.forDigit(shiftedDigit, 10);
-        } else if (Character.isLowerCase(ch)) { // Karakter bir küçük harfse
-            int offset = 'a'; // ASCII'de küçük harfler a'dan başlar
-            int shiftedChar = ((ch - offset + shiftValue) % 26 + 26) % 26 + offset; // Küçük harfler arasında bir döngü oluştur
-            return (char) shiftedChar;
-        } else if (Character.isUpperCase(ch)) { // Karakter bir büyük harfse
-            int offset = 'A'; // ASCII'de büyük harfler A'dan başlar
-            int shiftedChar = ((ch - offset + shiftValue) % 26 + 26) % 26 + offset; // Büyük harfler arasında bir döngü oluştur
-            return (char) shiftedChar;
-        } else { // Karakter bir harf veya sayı değilse
-            throw new IllegalArgumentException("Invalid character: " + ch);
-        }
-    }
+    
 
 }
